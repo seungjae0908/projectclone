503c408c3ba09435336284e1cc2d7b949e785345
kyoungmin <stephen4951@gmail.com>
2024-05-08 15:40:01 +0900

    Cart Context와 Reducer로 전역 상태 관리, 모달 열고 닫기 처리

--- src/components/UI/Modal/CartModal.js
+++ src/components/UI/Modal/CartModal.js
@@ -2,8 +2,10 @@ import React from 'react';
 import styles from './CartModal.module.scss';
 import Portal from '../Portal/Portal';
 
-const BackDrop = () => {
-  return <div className={styles.backdrop} />;
+const BackDrop = ({ onClose }) => {
+  return (
+    <div className={styles.backdrop} onClick={onClose} />
+  );
 };
 
 const ModalOverlay = ({ children }) => {
@@ -14,11 +16,11 @@ const ModalOverlay = ({ children }) => {
   );
 };
 
-const CartModal = ({ children }) => {
+const CartModal = ({ children, onClose }) => {
   return (
     <>
       <Portal destId='backdrop-root'>
-        <BackDrop />
+        <BackDrop onClose={onClose} />
       </Portal>
       <Portal destId='overlay-root'>
         <ModalOverlay>{children}</ModalOverlay>
