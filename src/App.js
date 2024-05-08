38b63de739f17c43f3dc07280a18cb18f352af34
kyoungmin <stephen4951@gmail.com>
2024-05-08 11:08:38 +0900

    Food 템플릿 작성 중 (Header 부분)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import './App.css';
-import React, {
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
-import MainHeader from './components/SideEffect/MainHeader/MainHeader';
-import Login from './components/SideEffect/Login/Login';
-import Home from './components/SideEffect/Home/Home';
-import AuthContext from './components/store/auth-context';
+import React from 'react';
+import Header from './components/Food/Layout/Header';
 
 const App = () => {
-  const { isLoggedIn } = useContext(AuthContext);
-
   return (
     <>
-      <MainHeader />
-      <main>
-        {isLoggedIn && <Home />}
-        {!isLoggedIn && <Login />}
-      </main>
+      <Header />
     </>
   );
 };
